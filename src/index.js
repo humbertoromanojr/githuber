00aa02285ec85acf38e2e8c71713c9b8d05e5551
HJr Dev <astronomi@gmail.com>
2019-05-12 21:58:35 -0300

    config Reactotron

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
 
+import "./config/ReactotronConfig";
+
 export default class App extends Component {
   render() {
     return (
