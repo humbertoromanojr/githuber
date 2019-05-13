253ce0c9f77347ddd86c1abeb940a6856652d2cc
HJr Dev <astronomi@gmail.com>
2019-05-12 22:05:57 -0300

    Reactotron atributed in console

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,5 +1,7 @@
 import Reactotron from "reactotron-react-native";
 
-Reactotron.configure({ host: "192.168.2.106" }) // controls connection & communication settings
+const tron = Reactotron.configure({ host: "192.168.2.106" }) // controls connection & communication settings
   .useReactNative() // add all built-in react native plugins
   .connect(); // let's connect!
+
+console.tron = tron;
