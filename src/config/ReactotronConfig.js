8b5f5c31c2b3ae4df6f3db1733b23c675232187d
HJr Dev <astronomi@gmail.com>
2019-05-12 22:09:44 -0300

    config Reactotron only works development

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,7 +1,9 @@
 import Reactotron from "reactotron-react-native";
 
-const tron = Reactotron.configure({ host: "192.168.2.106" }) // controls connection & communication settings
-  .useReactNative() // add all built-in react native plugins
-  .connect(); // let's connect!
+if (__DEV__) {
+  const tron = Reactotron.configure({ host: "192.168.2.106" }) // controls connection & communication settings
+    .useReactNative() // add all built-in react native plugins
+    .connect(); // let's connect!
 
-console.tron = tron;
+  console.tron = tron;
+}
