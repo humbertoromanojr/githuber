5726f2593258ba6cd20bc5a48fad26789da094f9
HJr Dev <astronomi@gmail.com>
2019-05-20 12:28:01 -0300

    changing Header Statefull

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Text, View, TouchableOpacity, StatusBar,
 } from 'react-native';
@@ -8,18 +8,22 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import PropTypes from 'prop-types';
 import styles from './styles';
 
-const Header = ({ title }) => (
-  <View style={styles.container}>
-    <StatusBar barStyle="dark-content" />
+class Header extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="dark-content" />
 
-    <View style={styles.left}>
-      <Text style={styles.title}>{title}</Text>
-    </View>
-    <TouchableOpacity onPress={() => {}}>
-      <Icon nsmr="exchange" size={16} style={styles.icon} />
-    </TouchableOpacity>
-  </View>
-);
+        <View style={styles.left}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+        <TouchableOpacity onPress={() => {}}>
+          <Icon nsmr="exchange" size={16} style={styles.icon} />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
