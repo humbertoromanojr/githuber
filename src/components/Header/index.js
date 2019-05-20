fde386f0d65e9bd56a6721e79b777091eb2787a5
HJr Dev <astronomi@gmail.com>
2019-05-20 12:48:26 -0300

    config signOut and navigation Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 import {
-  Text, View, TouchableOpacity, StatusBar,
+  Text, View, TouchableOpacity, StatusBar, AsyncStorage,
 } from 'react-native';
 
+import { withNavigation } from 'react-navigation';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import PropTypes from 'prop-types';
 import styles from './styles';
 
 class Header extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+  };
+
+  signOut = async () => {
+    const { navigation } = this.this.props;
+
+    await AsyncStorage.clear();
+
+    navigation.navigate('Welcome');
+  };
+
   render() {
+    const { title } = this.props;
+
     return (
       <View style={styles.container}>
         <StatusBar barStyle="dark-content" />
@@ -17,16 +32,12 @@ class Header extends Component {
         <View style={styles.left}>
           <Text style={styles.title}>{title}</Text>
         </View>
-        <TouchableOpacity onPress={() => {}}>
-          <Icon nsmr="exchange" size={16} style={styles.icon} />
+        <TouchableOpacity onPress={this.signOut}>
+          <Icon name="exchange" size={16} style={styles.icon} />
         </TouchableOpacity>
       </View>
     );
   }
 }
 
-Header.propTypes = {
-  title: PropTypes.string.isRequired,
-};
-
-export default Header;
+export default withNavigation(Header);
