76ed2107396f89bbbc6353bb67c437b2478ca7c4
HJr Dev <astronomi@gmail.com>
2019-05-19 22:00:30 -0300

    using ComponentDidMount

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { AsyncStorage } from 'react-native'
 import './config/ReactotronConfig';
 
 import Routes from './routes';
@@ -9,6 +9,20 @@ export default class App extends Component {
     userChecked: false,
     userLogged: false,
   };
+
+  componentDidMount() {
+    const userName = await AsyncStorage.getItem('@Githuber:userName')
+
+    this.setState({
+      userChecked: true,
+      /**
+       * !! transforma em true se existir, senão fica falso
+       *
+       * !! true if it exists, otherwise false
+       * */
+      userLogged: !!userName
+    })
+  }
 
   render() {
     return <Routes />;
