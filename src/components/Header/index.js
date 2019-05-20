b90c6c1cc81ff8ef618254461c7b3e76751c3796
HJr Dev <astronomi@gmail.com>
2019-05-20 12:22:03 -0300

    use Vector Icons

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import {
+  Text, View, TouchableOpacity, StatusBar,
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import PropTypes from 'prop-types';
 import styles from './styles';
 
 const Header = ({ title }) => (
   <View style={styles.container}>
+    <StatusBar barStyle="dark-content" />
+
     <View style={styles.left}>
       <Text style={styles.title}>{title}</Text>
     </View>
-    <TouchableOpacity onPress={() => {}} />
+    <TouchableOpacity onPress={() => {}}>
+      <Icon nsmr="exchange" size={16} style={styles.icon} />
+    </TouchableOpacity>
   </View>
 );
 
