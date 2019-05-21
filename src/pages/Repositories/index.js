bef2e478ac1885e59cc823931d7bef48a2ca645b
HJr Dev <astronomi@gmail.com>
2019-05-20 23:59:37 -0300

    configuring Loding on the Repositories page

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text, AsyncStorage } from 'react-native';
+import {
+  View, Text, AsyncStorage, ActivityIndicator,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import PropTypes from 'prop-types';
 import api from '~/services/api';
 
 import Header from '~/components/Header';
+import styles from './styles';
 
 const TabIcon = ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />;
 
@@ -19,22 +22,25 @@ export default class Repositories extends Component {
 
   state = {
     data: [],
+    loading: true,
   };
 
   async componentDidMount() {
     const userName = await AsyncStorage.getItem('@Githuber:userName');
     const response = await api.get(`/users/${userName}/repos`);
 
-    this.setState({ data: response.data });
+    this.setState({ data: response.data, loading: false });
   }
 
+  renderList = () => <Text>Lista</Text>;
+
   render() {
+    const { loading } = this.state;
+
     return (
       <View>
         <Header title="Repositórios" />
-        {this.state.data.map(repo => (
-          <Text>{repo.name}</Text>
-        ))}
+        {loading ? <ActivityIndicator style={styles.loading} /> : this.renderList()}
       </View>
     );
   }
