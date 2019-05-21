55b4f8287ae686ac8089e5c49a3951cbf577a24a
HJr Dev <astronomi@gmail.com>
2019-05-20 23:45:41 -0300

    config API

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text, AsyncStorage } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import PropTypes from 'prop-types';
+import api from '~/services/api';
+
 import Header from '~/components/Header';
 
 const TabIcon = ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />;
@@ -15,10 +17,24 @@ export default class Repositories extends Component {
     tabBarIcon: TabIcon,
   };
 
+  state = {
+    data: [],
+  };
+
+  async componentDidMount() {
+    const userName = await AsyncStorage.getItem('@Githuber:userName');
+    const response = await api.get(`/users/${userName}/repos`);
+
+    this.setState({ data: response.data });
+  }
+
   render() {
     return (
       <View>
         <Header title="Repositórios" />
+        {this.state.data.map(repo => (
+          <Text>{repo.name}</Text>
+        ))}
       </View>
     );
   }
